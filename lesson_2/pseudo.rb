5b3a7399292e8861ad1d2b8be746f74436058ece
Aaron Sawyer <me@aarondevon.com>
2016-01-04 14:02:42 -0600

    Move pseudo.rb to pseudo.txt

---
+++ lesson_2/pseudo.rb
@@ -0,0 +1,49 @@
+# Write out pseudo-code (both casual and formal) that does the following:
+
+# 1. a method that returns the sum of two integers
+# 2. a method that takes an array of strings, and returns a string that is all those strings concatenated together
+# 3. a method that takes an array of integers, and returns a new array with every other element
+
+# Casual
+# Create a method
+# request first number from the user and assign to a variable
+# request second number from the user and assign to a variable
+# add the two variables together
+# print the result
+
+# Formal
+
+# Start
+
+# method sum
+# PRINT a request to user for the first number
+# SET num1 = GET
+# PRINT a request to user for the second number
+# SET num2 = GET
+# SET total = num1 + num2
+# PRINT total
+
+#END
+
+# Casual
+# Given an array of strings
+# Itterate through the array one by one
+# Take the first index and assign it to a variable
+# Take each additionl index and add it to the array
+# Print the variable
+
+#Formal
+
+#START
+
+#GIVEN an array of strings called str
+
+# SET i to 0
+# WHILE i < str.length
+  # SET
+
+
+
+
+
+
