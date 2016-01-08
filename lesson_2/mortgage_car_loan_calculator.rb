6cc7d349dffb5f642c63a305e6475cfa0d918bec
Aaron Sawyer <me@aarondevon.com>
2016-01-08 12:53:23 -0600

    Request name and type of loan

---
+++ lesson_2/mortgage_car_loan_calculator.rb
@@ -0,0 +1,30 @@
+puts "Hi! Welcome to the Mortgage and Auto Loan Calculator."
+puts "Please enter your first name."
+name = gets.chomp
+puts "Great #{name}! Let's get started."
+puts "In order to help you I will need to collect some information.\nFirst, is this a mortgage or auto loan?\nFor mortgage type 1\nFor auto type 2"
+auto_mortgage = gets.chomp
+
+confirm = false
+while confirm == false
+  if auto_mortgage == "1"
+    puts "You selected mortgage, is that correct?\nFor Yes type 1\nFor No type 2"
+    y_or_n = gets.chomp
+    if y_or_n == "1"
+      break
+    else
+      puts "Okay, What woud you like then?\nFor mortgage type 1\nFor auto type 2"
+      auto_mortgage = gets.chomp
+    end
+
+  elsif auto_mortgage == "2"
+    puts "You selected auto, is that correct\nFor Yes type 1\nFor No type 2"
+    y_or_n = gets.chomp
+    if y_or_n == "1"
+      break
+    else
+      puts "Okay, What woud you like then?\nFor mortgage type 1\nFor auto type 2"
+      auto_mortgage = gets.chomp
+    end
+  end
+end
