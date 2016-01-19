9e66933ed4cf20447d3081a084ea148dc1d9e3de
Aaron Sawyer <me@aarondevon.com>
2016-01-19 23:37:54 +0000

    Add validation

--- lesson_2/mortgage_car_loan_calculator.rb
+++ lesson_2/mortgage_car_loan_calculator.rb
@@ -11,11 +11,10 @@ puts "Hi! Welcome to the Mortgage and Auto Loan Calculator."
 puts "Please enter your first name."
 name = gets.chomp
 
-loop do 
+loop do
   puts "Great #{name}! Let's get started."
   puts "In order to help you I will need to collect some information.\nFirst, is this a mortgage or auto loan?\nFor mortgage type 1\nFor auto type 2"
   auto_mortgage = gets.chomp
-  
   confirm = false
   while confirm == false
     if auto_mortgage == "1"
@@ -23,20 +22,26 @@ loop do
       y_or_n = gets.chomp
       if y_or_n == "1"
         confirm = true
-      else
+      elsif y_or_n == "2"
         puts "Okay, What woud you like then?\nFor mortgage type 1\nFor auto type 2"
         auto_mortgage = gets.chomp
+      else
+        puts "Sorry, That is not a valid option."
       end
-  
     elsif auto_mortgage == "2"
       puts "You selected auto, is that correct\nFor Yes type 1\nFor No type 2"
       y_or_n = gets.chomp
       if y_or_n == "1"
         confirm = true
-      else
+      elsif y_or_n == "2"
         puts "Okay, What woud you like then?\nFor mortgage type 1\nFor auto type 2"
         auto_mortgage = gets.chomp
+      else
+        puts "Sorry, That is not a valid option."
       end
+    else
+      puts "Sorry, That is not a valid option.\nauto loan?\nFor mortgage type 1\nFor auto type 2"
+      auto_mortgage = gets.chomp
     end
   end
   puts "Please enter the loan amount #{name}"
@@ -46,9 +51,7 @@ loop do
   puts "And finally I will need the APR"
   puts "Example for 5% 5"
   interest_amount = gets.chomp.to_f / 100 / 12
-  
   puts "Your monthly payment will be $#{fixed_monthly_payment(loan_amount, interest_amount, loan_duration_in_months)}"
-  
   puts "Would you like to enter another loan?\nFor Yes type 1\nFor No type 2"
   another_loan = gets.chomp
   if another_loan == "2"
