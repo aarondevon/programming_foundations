2827df88842c9133ae32c04a0a1271bb6892debc
aarondevon <me@aarondevon.com>
2016-05-10 11:30:19 -0500

    Complete question 2

--- lesson_3/exercise-easy-2.rb
+++ lesson_3/exercise-easy-2.rb
@@ -1,17 +1,15 @@
-# Question 1
+#Question 2
 
- # In this hash of people and their age,
+  # Add up all of the ages from our current Munster family hash:
 
-  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
-  # see if there is an age present for "Spot".
+  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
 
-  p ages.has_key?("Spot")
+def munster_family_total_ages(ages_hash)
+  total_age = 0
 
- # Bonus: What are two other hash methods that would work just as well for this solution?
+  ages_hash.each_value { |age|  total_age += age }
 
- p ages.include?("Spot")
+  total_age
+end
 
- # this method is not that great since it throws an error if the key isn't found
- # p ages.fetch("Spot")
-
- p ages.member?("Spot")
+puts munster_family_total_ages(ages)
