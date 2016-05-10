9f8018ed3e24839be92b64bf5a3e9505f81f1261
aarondevon <me@aarondevon.com>
2016-05-10 11:35:15 -0500

    Complete question 2

--- lesson_3/exercise-easy-2.rb
+++ lesson_3/exercise-easy-2.rb
@@ -15,3 +15,19 @@
  # p ages.fetch("Spot")
 
  p ages.member?("Spot")
+
+#Question 2
+
+  # Add up all of the ages from our current Munster family hash:
+
+  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+  def munster_family_total_ages(ages_hash)
+    total_age = 0
+
+    ages_hash.each_value { |age|  total_age += age }
+
+    total_age
+  end
+
+  puts munster_family_total_ages(ages)
