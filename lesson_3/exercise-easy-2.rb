fb319979dcf416969236d1375494be9415854a83
aarondevon <me@aarondevon.com>
2016-05-14 16:32:35 -0500

    complete question 4

--- lesson_3/exercise-easy-2.rb
+++ lesson_3/exercise-easy-2.rb
@@ -42,3 +42,22 @@
   ages.select! do |key, value|
     value < 100
   end
+
+#Question 4
+
+  # Starting with this string:
+
+  munsters_description = "The Munsters are creepy in a good way."
+  # Convert the string in the following ways (code will be executed on original munsters_description above):
+
+  # "The munsters are creepy in a good way."
+  puts munsters_description.capitalize
+
+  # "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+  puts munsters_description.swapcase
+
+  # "the munsters are creepy in a good way."
+  puts munsters_description.downcase
+
+  # "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+  puts munsters_description.upcase
