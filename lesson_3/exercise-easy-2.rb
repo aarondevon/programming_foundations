e8d9179b8c7e5a6d4f242b906768e0a6f02b0ed0
aarondevon <me@aarondevon.com>
2016-05-14 15:40:36 -0500

    Complete question 3

--- lesson_3/exercise-easy-2.rb
+++ lesson_3/exercise-easy-2.rb
@@ -1,33 +1,10 @@
-# Question 1
+# Question 3
 
- # In this hash of people and their age,
+# In the age hash:
 
-  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
-  # see if there is an age present for "Spot".
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+# throw out the really old people (age 100 or older).
 
-  p ages.has_key?("Spot")
-
- # Bonus: What are two other hash methods that would work just as well for this solution?
-
- p ages.include?("Spot")
-
- # this method is not that great since it throws an error if the key isn't found
- # p ages.fetch("Spot")
-
- p ages.member?("Spot")
-
-#Question 2
-
-  # Add up all of the ages from our current Munster family hash:
-
-  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
-
-  def munster_family_total_ages(ages_hash)
-    total_age = 0
-
-    ages_hash.each_value { |age|  total_age += age }
-
-    total_age
-  end
-
-  puts munster_family_total_ages(ages)
+ages.select! do |key, value|
+  value < 100
+end
