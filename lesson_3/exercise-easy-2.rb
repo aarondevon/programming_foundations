11076c9e6981859efededda5d188f4afff197d16
aarondevon <me@aarondevon.com>
2016-05-14 19:50:56 -0500

    complete question 5

--- lesson_3/exercise-easy-2.rb
+++ lesson_3/exercise-easy-2.rb
@@ -1,63 +1,10 @@
-# Question 1
+# Question 5
 
- # In this hash of people and their age,
+  # We have most of the Munster family in our age hash:
 
-  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
-  # see if there is an age present for "Spot".
+  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+  # add ages for Marilyn and Spot to the existing hash
 
-  p ages.has_key?("Spot")
-
- # Bonus: What are two other hash methods that would work just as well for this solution?
-
- p ages.include?("Spot")
-
- # this method is not that great since it throws an error if the key isn't found
- # p ages.fetch("Spot")
-
- p ages.member?("Spot")
-
-#Question 2
-
-  # Add up all of the ages from our current Munster family hash:
-
-  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
-
-  def munster_family_total_ages(ages_hash)
-    total_age = 0
-
-    ages_hash.each_value { |age|  total_age += age }
-
-    total_age
-  end
-
-  puts munster_family_total_ages(ages)
-
-# Question 3
-
-  # In the age hash:
-
-  ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
-  # throw out the really old people (age 100 or older).
-
-  ages.select! do |key, value|
-    value < 100
-  end
-
-#Question 4
-
-  # Starting with this string:
-
-  munsters_description = "The Munsters are creepy in a good way."
-  # Convert the string in the following ways (code will be executed on original munsters_description above):
-
-  # "The munsters are creepy in a good way."
-  puts munsters_description.capitalize
-
-  # "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
-  puts munsters_description.swapcase
-
-  # "the munsters are creepy in a good way."
-  puts munsters_description.downcase
-
-  # "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
-  puts munsters_description.upcase
+  additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+  ages.merge!(additional_ages)
+  p ages
